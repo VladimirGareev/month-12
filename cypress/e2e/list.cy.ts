4a024f6620383203fc13577c757784e5decec4a8
Vladimir Gareev <vladimir.R.gareev@yandex.ru>
2023-06-02 20:44:38 +0500

    ready for check

--- cypress/e2e/list.cy.ts
+++ cypress/e2e/list.cy.ts
@@ -1,13 +1,12 @@
 describe("string page testing", () => {
-    beforeEach(() => cy.visit("http://localhost:3000/list"));
+  beforeEach(() => cy.visit("http://localhost:3000/list"));
 
     it('если в инпуте текста пусто - кнопки "Добавить в head", "Добавить в Tail" и "Добавить по индексу" неактивны', () => {
       cy.get("input").eq(0).should("be.empty");
       cy.get('[data-testid="button"]').eq(0).should("be.disabled");
       cy.get('[data-testid="button"]').eq(1).should("be.disabled");
       cy.get('[data-testid="button"]').eq(4).should("be.disabled");
-
-    })
+    });
 
     it('если в инпуте индекса пусто - кнопки "Добавить по индексу" и "Удалить по ндексу" неактивны', () => {
       cy.get("input").eq(1).should("be.empty");
@@ -15,72 +14,200 @@ describe("string page testing", () => {
       cy.get('[data-testid="button"]').eq(5).should("be.disabled");
       cy.get('[data-testid="button"]').eq(4).should("be.disabled");
     });
-    
-    it('проверка отрисовки дефолтного списка ARRAY', () => {
-        cy.get('[data-testid="small"]').should('have.length', 5);
-        cy.get('[data-testid="small"]').each((element, index)=>{
-            switch(index){
-                case 0: 
-                cy.wrap(element).should('contain', 'A').get('[data-testid="head"]').should('contain','head');
-                cy.wrap(element).get('[data-testid="tail"]').should('be.empty');
-                break;
-                case 1: 
-                cy.wrap(element).should('contain', 'R').get('[data-testid="head"]').should('be.empty');
-                cy.wrap(element).get('[data-testid="tail"]').should('be.empty');
-                break;
-                case 2: 
-                cy.wrap(element).should('contain', 'R').get('[data-testid="head"]').should('be.empty');
-                cy.wrap(element).get('[data-testid="tail"]').should('be.empty');
-                break;
-                case 3: 
-                cy.wrap(element).should('contain', 'A').get('[data-testid="head"]').should('be.empty');
-                cy.wrap(element).get('[data-testid="tail"]').should('be.empty');
-                break;
-                case 4: 
-                cy.wrap(element).should('contain', 'Y').get('[data-testid="head"]').should('be.empty');
-                cy.wrap(element).get('[data-testid="tail"]').should('contain','tail');
-                break;
-            }
+
+    it("проверка отрисовки дефолтного списка ARRAY", () => {
+      cy.get('[data-testid="small"]').should("have.length", 5);
+      cy.get('[data-testid="small"]').each((element, index) => {
+        switch (index) {
+          case 0:
+            cy.wrap(element)
+              .should("contain", "A")
+              .get('[data-testid="head"]')
+              .should("contain", "head");
+            cy.wrap(element).get('[data-testid="tail"]').should("be.empty");
+            break;
+          case 1:
+            cy.wrap(element)
+              .should("contain", "R")
+              .get('[data-testid="head"]')
+              .should("be.empty");
+            cy.wrap(element).get('[data-testid="tail"]').should("be.empty");
+            break;
+          case 2:
+            cy.wrap(element)
+              .should("contain", "R")
+              .get('[data-testid="head"]')
+              .should("be.empty");
+            cy.wrap(element).get('[data-testid="tail"]').should("be.empty");
+            break;
+          case 3:
+            cy.wrap(element)
+              .should("contain", "A")
+              .get('[data-testid="head"]')
+              .should("be.empty");
+            cy.wrap(element).get('[data-testid="tail"]').should("be.empty");
+            break;
+          case 4:
+            cy.wrap(element)
+              .should("contain", "Y")
+              .get('[data-testid="head"]')
+              .should("be.empty");
+            cy.wrap(element)
+              .get('[data-testid="tail"]')
+              .should("contain", "tail");
+            break;
+        }
+      });
+    });
+
+    it("проверка добавления элемента в HEAD", () => {
+      cy.get("input").eq(0).type("S");
+      cy.get('[data-testid="button"]').eq(0).click();
+      cy.get('[data-testid="head"]')
+        .eq(0)
+        .within(() => {
+          cy.get('[data-testid="small"]')
+            .should("contain", "S")
+            .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+        });
+      cy.get('[data-testid="head"]').eq(0).should("contain", "head");
+      cy.get('[data-testid="small"]')
+        .should("have.length", 6)
+        .eq(0)
+        .should("contain", "S");
+    });
+
+    it("проверка добавления элемента в TAIL", () => {
+      cy.get("input").eq(0).type("S");
+      cy.get('[data-testid="button"]').eq(1).click();
+      cy.get('[data-testid="head"]')
+        .eq(5)
+        .within(() => {
+          cy.get('[data-testid="small"]')
+            .should("contain", "S")
+            .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+        });
+      cy.get('[data-testid="head"]').eq(5).should("be.empty");
+      cy.get('[data-testid="small"]')
+        .should("have.length", 6)
+        .eq(5)
+        .should("contain", "S");
+    });
+
+    it("проверка добавления элемента по индексу", () => {
+      cy.get("input").eq(0).type("S");
+      cy.get("input").eq(1).type("2");
+      cy.get('[data-testid="button"]').eq(4).click();
+      cy.get('[data-testid="head"]')
+        .eq(0)
+        .within(() => {
+          cy.get('[data-testid="small"]')
+            .should("contain", "S")
+            .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+        });
+      cy.get('[data-testid="head"]')
+        .eq(1)
+        .within(() => {
+          cy.get('[data-testid="small"]')
+            .should("contain", "S")
+            .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+        });
+      cy.get('[data-testid="head"]')
+        .eq(2)
+        .within(() => {
+          cy.get('[data-testid="small"]')
+            .should("contain", "S")
+            .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+        });
+      cy.get('[data-testid="circle"]').eq(2).should("contain", "S");
+      cy.get('[data-testid="circle"]').should("have.length", 6);
+    });
+
+    it("проверка удаления элемента из head", () => {
+      cy.get('[data-testid="button"]').eq(2).click();
+      cy.get('[data-testid="circle"]')
+        .eq(0)
+        .within(() => {
+          cy.get("p").should("be.empty");
+          cy.get('[data-testid="tail"]')
+            .eq(0)
+            .should("contain", "A")
+            .within(() => {
+              cy.get('[data-testid="small"]').and(
+                "have.css",
+                "border",
+                "3.84px solid rgb(210, 82, 225)"
+              );
+            });
+        });
+      cy.get('[data-testid="circle"]')
+        .should("have.length", 4)
+        .eq(0)
+        .should("contain", "R").within(()=>{
+          cy.get('[data-testid="head"]').should('contain', 'head');
         })
+    });
+    it("проверка удаления элемента из tail", () => {
+      cy.get('[data-testid="button"]').eq(3).click();
+      cy.get('[data-testid="small"]')
+        .eq(0)
+        .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+      cy.get('[data-testid="small"]')
+        .eq(1)
+        .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+      cy.get('[data-testid="small"]')
+        .eq(2)
+        .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+      cy.get('[data-testid="small"]')
+        .eq(3)
+        .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+      cy.get('[data-testid="circle"]')
+        .eq(4)
+        .within(() => {
+          cy.get('[data-testid="tail"]')
+            .eq(0)
+            .should("contain", "Y")
+            .within(() => {
+              cy.get('[data-testid="small"]').and(
+                "have.css",
+                "border",
+                "3.84px solid rgb(210, 82, 225)"
+              );
+            });
+          cy.get("p").should("be.empty");
+        });
+        cy.get('[data-testid="circle"]').should('have.length',4).eq(3).within(()=>{
+          cy.get('[data-testid="tail"]').should('contain','tail');
+        })
+    });
 
-  });
-
-  
-  it('проверка добавления элемента в HEAD', () => {
-    cy.get("input").eq(0).type("S");
-    cy.get('[data-testid="button"]').eq(0).click();
-    cy.get('[data-testid="head"]').eq(0).within(()=>{
-        cy.get('[data-testid="small"]').should('contain', 'S').should('have.css','border',"3.84px solid rgb(210, 82, 225)");
-    })
-    cy.get('[data-testid="head"]').eq(0).should('contain', 'head');
-    cy.get('[data-testid="small"]').should('have.length', 6).eq(0).should('contain', 'S');
-});
-
-it('проверка добавления элемента в TAIL', () => {
-    cy.get("input").eq(0).type("S");
-    cy.get('[data-testid="button"]').eq(1).click();
-    cy.get('[data-testid="head"]').eq(5).within(()=>{
-        cy.get('[data-testid="small"]').should('contain', 'S').should('have.css','border',"3.84px solid rgb(210, 82, 225)");
-    })
-    cy.get('[data-testid="head"]').eq(5).should('be.empty');
-    cy.get('[data-testid="small"]').should('have.length', 6).eq(5).should('contain', 'S');
-});
-
-it('проверка добавления элемента по индексу', () => {
-    cy.get("input").eq(0).type("S");
+  it("проверка удаления элемента по индексу", () => {
     cy.get("input").eq(1).type("2");
-    cy.get('[data-testid="button"]').eq(4).click();
-    cy.get('[data-testid="head"]').eq(0).within(()=>{
-        cy.get('[data-testid="small"]').should('contain', 'S').should('have.css','border',"3.84px solid rgb(210, 82, 225)");
-    })
-    cy.get('[data-testid="head"]').eq(1).within(()=>{
-        cy.get('[data-testid="small"]').should('contain', 'S').should('have.css','border',"3.84px solid rgb(210, 82, 225)");
-    })
-    cy.get('[data-testid="head"]').eq(2).within(()=>{
-        cy.get('[data-testid="small"]').should('contain', 'S').should('have.css','border',"3.84px solid rgb(210, 82, 225)");
-    })
-    cy.get('[data-testid="circle"]').eq(2).should('contain', 'S');
+    cy.get('[data-testid="button"]').eq(5).click();
+    cy.get('[data-testid="small"]')
+      .eq(0)
+      .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+    cy.get('[data-testid="small"]')
+      .eq(1)
+      .should("have.css", "border", "3.84px solid rgb(210, 82, 225)");
+    cy.get('[data-testid="circle"]')
+      .eq(2)
+      .within(() => {
+        cy.get('[data-testid="tail"]')
+          .eq(0)
+          .should("contain", "R")
+          .within(() => {
+            cy.get('[data-testid="small"]').and(
+              "have.css",
+              "border",
+              "3.84px solid rgb(210, 82, 225)"
+            );
+          });
+        cy.get("p").should("be.empty");
+      });
+    cy.get('[data-testid="circle"]')
+      .should("have.length", 4)
+      .eq(2)
+      .should("contain", "A");
+  });
 });
-
-})
-  
--- cypress/e2e/routing.cy.ts
+++ cypress/e2e/routing.cy.ts
@@ -1,29 +1,29 @@
-describe ("сайт запускается, роутинг работает", ()=>{
-    it("сайт запускается на порте 3000", ()=>{
-        cy.visit('http://localhost:3000');
-    })
-    it("страница с разворотом строки открывается", ()=>{
-        cy.visit('http://localhost:3000/recursion');
-        cy.get('button').contains('К оглавлению').click();
-    })
-    it("страница с числами фибоначчи открывается", ()=>{
-        cy.visit('http://localhost:3000/fibonacci');
-        cy.get('button').contains('К оглавлению').click();
-    })
-    it("страница с сортировкой массива открывается", ()=>{
-        cy.visit('http://localhost:3000/sorting');
-        cy.get('button').contains('К оглавлению').click();
-    })
-    it("страница со стаком открывается", ()=>{
-        cy.visit('http://localhost:3000/stack');
-        cy.get('button').contains('К оглавлению').click();
-    })
-    it("страница с очередью открывается", ()=>{
-        cy.visit('http://localhost:3000/queue');
-        cy.get('button').contains('К оглавлению').click();
-    })
-    it("страница со связным списком открывается", ()=>{
-        cy.visit('http://localhost:3000/list');
-        cy.get('button').contains('К оглавлению').click();
-    })
-})
+describe("сайт запускается, роутинг работает", () => {
+  it("сайт запускается на порте 3000", () => {
+    cy.visit("http://localhost:3000");
+  });
+  it("страница с разворотом строки открывается", () => {
+    cy.visit("http://localhost:3000/recursion");
+    cy.get("button").contains("К оглавлению").click();
+  });
+  it("страница с числами фибоначчи открывается", () => {
+    cy.visit("http://localhost:3000/fibonacci");
+    cy.get("button").contains("К оглавлению").click();
+  });
+  it("страница с сортировкой массива открывается", () => {
+    cy.visit("http://localhost:3000/sorting");
+    cy.get("button").contains("К оглавлению").click();
+  });
+  it("страница со стаком открывается", () => {
+    cy.visit("http://localhost:3000/stack");
+    cy.get("button").contains("К оглавлению").click();
+  });
+  it("страница с очередью открывается", () => {
+    cy.visit("http://localhost:3000/queue");
+    cy.get("button").contains("К оглавлению").click();
+  });
+  it("страница со связным списком открывается", () => {
+    cy.visit("http://localhost:3000/list");
+    cy.get("button").contains("К оглавлению").click();
+  });
+});
